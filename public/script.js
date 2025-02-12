dc1ce1b9a94d1be43a3fc00740e223fa8fa53ae9
addyng <avn011@ucsd.edu>
2025-02-12 12:57:30 -0800

    added all form options

--- public/script.js
+++ public/script.js
@@ -1,28 +1,60 @@
-// const fileUpload = document.getElementById('fileUpload');
-// const content = document.getElementById('content');
-// const uploadForm = document.getElementById('uploadForm');
-// const submitButton = document.getElementById('submitButton');
-
-// fileUpload.addEventListener('change', (event) => {
-//     if (event.target.files[0]) {
-//         const newDiv = document.createElement('div');
-//         newDiv.id = "selectedFile";
-//         newDiv.textContent = "You selected " + event.target.files[0].name;
-//         uploadForm.insertBefore(newDiv, uploadForm.firstChild);
-//     }
-// });
-
-// submitButton.addEventListener('click', (event) => {
-//     if (!fileUpload.files[0]) {
-//         alert('Select a file please.');
-//     }
-//     fileUpload.reset();
-// })
-
 const h5ad_input = document.getElementById('h5ad_input');
 const selected_file = document.getElementById('selected_file');
+const celltype_label = document.getElementById('celltype_label');
+const auto_generated_colors = document.getElementById('auto_generated_colors');
+const celltype_hidden = document.querySelectorAll('.celltype_hidden');
+const not_auto_colors = document.querySelectorAll('.not_auto_colors');
+const raw_gene_counts = document.getElementById('raw_gene_counts');
+const raw_gene_hidden = document.querySelectorAll('.raw_gene_hidden');
 
 h5ad_input.addEventListener('change', (event) => {
     selected_file.textContent = "File selected: " + event.target.files[0].name;
 });
 
+celltype_label.addEventListener('click', (event) => {
+    if (celltype_label.checked) {
+        // Make options unhidden
+        celltype_hidden.forEach((element) => {
+            element.classList.add('show');
+        });
+    }
+
+    if (!celltype_label.checked) {
+        // Make options unhidden
+        celltype_hidden.forEach((element) => {
+            element.classList.remove('show');
+        });
+    }
+});
+
+auto_generated_colors.addEventListener('click', (event) => {
+    if (auto_generated_colors.checked) {
+        // Make options unhidden
+        not_auto_colors.forEach((element) => {
+            element.style = "display: none;";
+        });
+    }
+
+    if (!auto_generated_colors.checked) {
+        // Make options unhidden
+        not_auto_colors.forEach((element) => {
+            element.style = "";
+        });
+    }
+});
+
+raw_gene_counts.addEventListener('click', (event) => {
+    if (raw_gene_counts.checked) {
+        // Make options unhidden
+        raw_gene_hidden.forEach((element) => {
+            element.classList.add('show');
+        });
+    }
+
+    if (!raw_gene_counts.checked) {
+        // Make options unhidden
+        raw_gene_hidden.forEach((element) => {
+            element.classList.remove('show');
+        });
+    }
+});
